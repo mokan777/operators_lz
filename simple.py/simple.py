1869d8a4dc5a98052a146ba3022b452fbbb31176
mokan777 <rota.podem@tutamail.com>
2024-10-16 17:16:17 +0300

    simple

---
+++ simple.py/simple.py
@@ -0,0 +1,46 @@
+
+a = int(input())
+
+
+if a == 1:
+    result = "Y"
+else:
+    result = "N"
+if a == 2:
+    result = "Y"
+else:
+    result = "N"
+if a == 3:
+    result = "Y"
+else:
+    result = "N"
+if a == 5:
+    result = "Y"
+else:
+    result = "N"
+if a == 7:
+    result = "Y"
+else:
+    result = "N"
+if a == 11:
+    result = "Y"
+else:
+    result = "N"
+if a == 13:
+    result = "Y"
+else:
+    result = "N"
+if a == 17:
+    result = "Y"
+else:
+    result = "N"
+if a == 19:
+    result = "Y"
+else:
+    result = "N"
+if a == 23:
+    result = "Y"
+else:
+    result = "N"
+    
+print(result)
